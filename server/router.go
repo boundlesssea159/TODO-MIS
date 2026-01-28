a88fbc96306bf75bdea7a054f5a2021f26185802
codi <ze-feng.xu@hp.com>
2026-01-28 16:30:56 +0800

    add environment to avoid expose secret && add health api

--- server/router.go
+++ server/router.go
@@ -2,11 +2,15 @@ package server
 
 import (
 	"TODO-MIS/adapter/driving/api"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(r *gin.Engine, todo *api.Todo) {
+	r.GET("/health", func(c *gin.Context) {
+		c.Status(http.StatusOK)
+	})
 	apiGroup := r.Group("/api/v1")
 	apiGroup.POST("/todo-items", todo.Create)
 	apiGroup.GET("/todo-items", todo.List)
