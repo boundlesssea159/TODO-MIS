85162ce4f57a4983f7b3c74bbf3c9af54046d8ba
boundlesssea <xuzefeng159@163.com>
2026-01-28 08:21:03 +0800

    create dockerfile

---
+++ sql/init.sql
@@ -0,0 +1,21 @@
+USE todo-mis;
+
+CREATE TABLE IF NOT EXISTS `todo_items` (
+    `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+    `title` varchar(100) NOT NULL,
+    `description` varchar(255) NOT NULL,
+    `status` tinyint(5) DEFAULT 0,
+    `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='todo items table';
+
+
+CREATE TABLE IF NOT EXISTS `users` (
+      `id` int NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+      `username` varchar(50) NOT NULL,
+      `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+      PRIMARY KEY (`id`),
+      UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='user table';
